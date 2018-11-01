52036d384defad971360eae31c55c660d072d11e
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-11-01 17:22:47 +0900

    Customized in order to use with ELSA

--- person_detect.py
+++ person_detect.py
@@ -1,26 +1,17 @@
 import cv2
-import sys
-import logging as log
-import datetime as dt
-from time import sleep
 
 face_default = "haarcascade_frontalface_default.xml"
-
 faceCascade_default = cv2.CascadeClassifier(face_default)
-
-
-# log.basicConfig(filename='webcam.log',level=log.INFO)
-
 video_capture = cv2.VideoCapture(0)
 
 anterior1 = 0
 cnt1 = 0
 
-while True:
+CAPTURED_FRAME = 0
+
+while CAPTURED_FRAME < 10:
     if not video_capture.isOpened():
-        print('Unable to load camera.')
-        sleep(5)
-        pass
+        raise EnvironmentError('Camera not available')
 
     # Capture frame-by-frame
     ret, frame = video_capture.read()
@@ -36,24 +27,18 @@ while True:
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces1:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     if anterior1 != len(faces1):
         anterior1 = len(faces1)
-        # log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
-        cnt1 = cnt1+1
+        cnt1 = cnt1 + 1
 
-        if cnt1 > 1: 
+        if cnt1 > 1:
             print("-------------face detected by default!")
+            CAPTURED_FRAME += 1
             cnt1 = 0
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
 # When everything is done, release the capture
+# TODO change this with server-send code: Integrate with total recognition
 video_capture.release()
 cv2.destroyAllWindows()
