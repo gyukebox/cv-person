862d305bcb08dcbdab7687d857985045ae75030d
hyunkyung12 <khc0275@naver.com>
2018-11-01 16:11:21 +0900

    Update and rename person_detect.py.py to person_detect.py

--- person_detect.py
+++ person_detect.py
@@ -5,10 +5,8 @@ import datetime as dt
 from time import sleep
 
 face_default = "haarcascade_frontalface_default.xml"
-face_alt = "haarcascade_frontalface_alt.xml"
 
 faceCascade_default = cv2.CascadeClassifier(face_default)
-faceCascade_alt = cv2.CascadeClassifier(face_alt)
 
 
 # log.basicConfig(filename='webcam.log',level=log.INFO)
@@ -16,9 +14,7 @@ faceCascade_alt = cv2.CascadeClassifier(face_alt)
 video_capture = cv2.VideoCapture(0)
 
 anterior1 = 0
-anterior2 = 0
 cnt1 = 0
-cnt2 = 0
 
 while True:
     if not video_capture.isOpened():
@@ -38,22 +34,9 @@ while True:
         minSize=(30, 30)
     )
 
-    faces2 = faceCascade_alt.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=15,
-        minSize=(30, 30)
-    )
-
-
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces1:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    for (x, y, w, h) in faces2:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-
-    # print('--------------faces length : {}'.format(len(faces)))
 
 
     if anterior1 != len(faces1):
@@ -61,18 +44,9 @@ while True:
         # log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
         cnt1 = cnt1+1
 
-        if cnt1 > 15: 
-            print("-------------face detected by default!!!!!!")
+        if cnt1 > 1: 
+            print("-------------face detected by default!")
             cnt1 = 0
-
-    if anterior2 != len(faces2):
-        anterior2 = len(faces2)
-        # log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
-        cnt2 = cnt2+1
-
-        if cnt2 > 15: 
-            print("=============face detected by alt!!!!")
-            cnt2 = 0
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
